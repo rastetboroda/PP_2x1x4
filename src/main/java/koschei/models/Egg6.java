e99569428c6cfc1bff6ac07e24a0fbc157f4c9e7
Виктор <oscaruser91@yandex.ru>
2021-12-28 02:54:20 +0300

    Решение 2.1.4

--- src/main/java/koschei/models/Egg6.java
+++ src/main/java/koschei/models/Egg6.java
@@ -1,9 +1,22 @@
 package koschei.models;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
 public class Egg6 {
 
-    @Override
+    private Needle7 needle;
+
     public String toString() {
-        return ", в яйце иголка " + "";
+        return ", в яйце иголка " + needle.toString();
+    }
+
+
+    @Autowired
+    public void setNeedle(Needle7 needle) {
+        this.needle = needle;
     }
 }
+
+
--- src/main/java/koschei/models/Needle7.java
+++ src/main/java/koschei/models/Needle7.java
@@ -1,9 +1,17 @@
 package koschei.models;
 
-public class Needle7 {
+import org.springframework.stereotype.Component;
 
-    @Override
-    public String toString() {
-        return ", смерть Кощея на игле :( " + "";
+    @Component
+    public class Needle7 {
+        private final Deth8 deth;
+
+        public Needle7(Deth8 death) {
+            this.deth = death;
+        }
+
+        @Override
+        public String toString() {
+            return ", смерть Кощея на игле :(";
+        }
     }
-}
